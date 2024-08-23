a01ba53756c8e625147d1a0586a0f59779a3ea7b
Rafael Garcia <TthanderBR@gmail.com>
2024-08-23 10:30:27 -0300

    Replace - Documentation
    Substituído o código main pelo refac.
    Feita uma pequena documentação.

--- Source/funcs.py
+++ Source/funcs.py
@@ -1,5 +1,5 @@
+#-# Json funcs, to read and write json files
 import json
-
 def write_json(file:str, json_dict:dict) -> None:
     with open(file, 'w') as f:
         f.write(json.dumps(json_dict, indent=4, sort_keys=True))
@@ -9,13 +9,13 @@ def read_json(file:str) -> dict:
         return json.load(f)
     
 
-
+#-# Function to convert RGB values ​​to HEX values
 def rgb_to_hex(red,green, blue):
     rgb = (red, green, blue)
     return '#%02x%02x%02x' % rgb
 
 
-
+#-# Function that returns a string formatted with today's date
 from datetime import date
 def get_today():
     day_today = date.today().day
--- Source/main.py
+++ Source/main.py
@@ -1,244 +1,282 @@
-# Import Tkinter
+# Importing the Tkinter module.
 import tkinter as tk
-from tkinter import ttk
+from tkinter  import ttk
 
-# Import SQLite
-import sqlite3 as sql
-
+# Importing the Custom Functions module.
 import funcs as fnc
 
 
+# Exception class.
+# Used when you hear an error when loading the Json configuration file.
+class LoadConfigError(Exception):
+    def __init__(self, message, errors):            
+        # Call the base class constructor with the parameters it needs.
+        super().__init__(message)
+            
+        self.errors = errors
 
 
 
-translate = {
-    "window_title"                  : '' ,
+# V Main Code V
+
+#-# Main window
+window      = tk.Tk()
+# Window sise
+win_width   = 500
+win_height  = 300
+
+def update_window():
+    global window, active_translation, win_width, win_height
+
+    window.title(active_translation['window_title'])
+    window.geometry(f"{win_width}x{win_height}")
+    window.resizable(False, False)
+
+def top_bar():
+    global window, active_translation
+    global set_locale_en, set_locale_pt_br
+    global frame_welcome, frame_new_sicknote
+
+    def cascademenu_sys(root:tk.Menu) -> tk.Menu:
+        # Creating a cascade menu with system options
+        topbar_sys_cascade_menu = tk.Menu (root, tearoff=False)
+        # |   Home   | Go to home page
+        # | Language | Cascade menu to set the localization system
+        # |----------|
+        # |   Exit   | Exit button
+
+        topbar_sys_cascade_menu.add_command   (   #-# Home button
+            label   = active_translation['topbar_sys_home.button'],
+            command = frame_welcome
+        )
+        topbar_sys_cascade_menu.add_cascade   (   #-# Language cascade menu
+            label   = active_translation['topbar_sys_lang.cascmenu'],
+            menu    = lang_submenu (root)
+        )
+        topbar_sys_cascade_menu.add_separator ()  #-# Separator
+        topbar_sys_cascade_menu.add_command   (   #-# Exit button
+            label   = active_translation['topbar_sys_exit.button'],
+            command = window.quit
+        )
+
+        return topbar_sys_cascade_menu
+
+    def lang_submenu(root:tk.Menu) -> tk.Menu:
+        # Create a menu with locations
+        topbar_lang_menu = tk.Menu (root, tearoff=False)
+        # |  en   | Set the localization in English
+        # | pt_br | Set the localization in Brazilian_Portuguese
+        
+        topbar_lang_menu.add_command ( #-# English locate button
+            label   = 'en',
+            command = set_locale_en
+        )
+        topbar_lang_menu.add_cascade ( #-# Brazilian_Portuguese button
+            label   = 'pt_br',
+            command = set_locale_pt_br
+        )
     
-    "topbar_sys.topmenu"            : '' ,
-    "topbar_sys_home.button"        : '' ,
-    "topbar_sys_lang.cascmenu"      : '' ,
-    "topbar_sys_exit.button"        : '' ,
+        return topbar_lang_menu
 
-    "topbar_sicknote.topmenu"       : '' ,
-    "topbar_sicknote_new.button"    : '' ,
+    def menu_sicknote(root:tk.Menu) -> tk.Menu:
+        # Creating a cascade menu with release of sick notes
+        topbar_sick_notes_menu = tk.Menu(root, tearoff=False)
+        # | New |
+        
+        topbar_sick_notes_menu.add_command(     #-# New sick note button
+            label   = active_translation['topbar_sicknote_new.button'],
+            command = frame_new_sicknote
+        )
 
-    "frame_welcome.msg"             : '' ,
-    "frame_sicknote_new.date_label" : '' ,
-}  
+        return topbar_sick_notes_menu
 
-translate_matrix = {
-    
+    # Creating the top menubar
+    top_bar = tk.Menu(window)
+
+    # Applying all cascade menus to the top menubar
+    top_bar.add_cascade(    #-# System cascade menu
+        label = active_translation['topbar_sys.topmenu'],
+        menu  = cascademenu_sys(top_bar)
+    )
+    top_bar.add_cascade(    #-# Sick notes cascade menu
+        label = active_translation['topbar_sicknote.topmenu'],
+        menu  = menu_sicknote(top_bar)
+    )
+
+    # Applying the menubar to the window
+    window.config(menu=top_bar)
+
+
+#-# Load and Save Json Config.
+config_file = 'config.json'
+app_version = '1.0.0'
+
+def load_config() -> None:
+    global app_version, translation_matrix, locale, config_file
+    global save_config, update_translation
+
+    # Try to open the file, if can't, call the function to create the json file.
+    try: fnc.read_json(config_file)
+    except FileNotFoundError:
+        save_config()
+
+    # After creating or opening the file, values ​​are assigned to its variables.
+    file = fnc.read_json(config_file)
+    if file['version'] == app_version:
+        locale = file['locale']
+        update_translation()
+    else: # If the file version is not compatible with the application version
+        raise LoadConfigError('The version of the config file is not compatible with the application')
+
+def save_config() -> None:
+    # Creates the json file if it does not exist.
+    # If it exists, write it with updated information
+    global app_version, locale, config_file
+
+    json_config = {
+        "version"   : app_version,
+        "locale"    : locale
+    }
+
+    fnc.write_json(config_file, json_config)
+
+
+#-# Translation
+translation_matrix = {
     "pt_br":{
-        "window_title"  : 'Sistema HF'              ,
+        "window_title"                  : 'Sistema HF'  ,
     
-        "topbar_sys.topmenu"        : 'Sistema'     ,
-        "topbar_sys_home.button"    : 'Home'        ,
-        "topbar_sys_lang.cascmenu"  : 'Idioma'      ,
-        "topbar_sys_exit.button"    : 'Sair'        ,
+        "topbar_sys.topmenu"            : 'Sistema'     ,
+        "topbar_sys_home.button"        : 'Home'        ,
+        "topbar_sys_lang.cascmenu"      : 'Idioma'      ,
+        "topbar_sys_exit.button"        : 'Sair'        ,
 
-        "topbar_sicknote.topmenu"    : 'Atestados'  ,
-        "topbar_sicknote_new.button" : 'Novo'       ,
+        "topbar_sicknote.topmenu"       : 'Atestados'   ,
+        "topbar_sicknote_new.button"    : 'Novo'        ,
 
-        "frame_welcome.msg" : 'Bem vindo'           ,
+        "frame_welcome.msg"             : 'Bem vindo'   ,
 
-        "frame_sicknote_new.date_label" : 'Data'    ,
+        "frame_sicknote_new.date_label" : 'Data'        ,
     },
 
     "en":{
-        "window_title"  : 'System HF'               ,
+        "window_title"                  : 'System HF'   ,
     
-        "topbar_sys.topmenu"        : 'System'      ,
-        "topbar_sys_home.button"    : 'Home'        ,
-        "topbar_sys_lang.cascmenu"  : 'Language'    ,
-        "topbar_sys_exit.button"    : 'Exit'        ,
+        "topbar_sys.topmenu"            : 'System'      ,
+        "topbar_sys_home.button"        : 'Home'        ,
+        "topbar_sys_lang.cascmenu"      : 'Language'    ,
+        "topbar_sys_exit.button"        : 'Exit'        ,
 
-        "topbar_sicknote.topmenu"    : 'Sick Notes' ,
-        "topbar_sicknote_new.button" : 'New'        ,
+        "topbar_sicknote.topmenu"       : 'Sick Notes'  ,
+        "topbar_sicknote_new.button"    : 'New'         ,
 
-        "frame_welcome.msg" : 'Welcome'             ,
+        "frame_welcome.msg"             : 'Welcome'     ,
 
-        "frame_sicknote_new.date_label" : 'Date'    ,
+        "frame_sicknote_new.date_label" : 'Date'        ,
     }
-
 }
+locale = 'en'
+active_translation = {}
 
-class Application():
-    def __init__(self) -> None:
+def set_locale_en():
+    global locale, update_translation
+    locale = 'en'
+    update_translation()
 
-        self.win_width :int = 500
-        self.win_height:int = 300
+def set_locale_pt_br():
+    global locale, update_translation
+    locale = 'pt_br'
+    update_translation()
 
-        self.active_frame:tk.Frame = None
-
-        self.window = tk.Tk()
-
-        self.set_locale_en()
-        self.start_window()
-        self.start_top_bar()
-        self.frame_welcome()
-
-        self.window.mainloop()
-
-    # Start Functions
-    def start_window(self):
-        self.window.title(translate['window_title'])
-        self.window.geometry(f"{self.win_width}x{self.win_height}")
-        self.window.resizable(False, False)
-
-    def start_top_bar(self):
-        def cascademenu_sys(root:tk.Menu) -> tk.Menu:
-            # Creating a cascade menu with system options
-            topbar_sys_cascade_menu = tk.Menu (root, tearoff=False)
-            # |   Home   | Go to home page
-            # | Language | Cascade menu to set the localization system
-            # |----------|
-            # |   Exit   | Exit button
-
-            topbar_sys_cascade_menu.add_command   (   #-# Home button
-                label   = translate['topbar_sys_home.button'],
-                command = self.frame_welcome
-            )
-            topbar_sys_cascade_menu.add_cascade   (   #-# Language cascade menu
-                label   = translate['topbar_sys_lang.cascmenu'],
-                menu    = lang_submenu (root)
-            )
-            topbar_sys_cascade_menu.add_separator ()  #-# Separator
-            topbar_sys_cascade_menu.add_command   (   #-# Exit button
-                label   = translate['topbar_sys_exit.button'],
-                command = self.window.quit
-            )
-
-            return topbar_sys_cascade_menu
-        
-        def lang_submenu(root:tk.Menu) -> tk.Menu:
-            # Create a menu with locations
-            topbar_lang_menu = tk.Menu (root, tearoff=False)
-            # |  en   | Set the localization in English
-            # | pt_br | Set the localization in Brazilian_Portuguese
-            
-            topbar_lang_menu.add_command ( #-# English locate button
-                label   = 'en',
-                command = self.set_locale_en
-            )
-            topbar_lang_menu.add_cascade ( #-# Brazilian_Portuguese button
-                label   = 'pt_br',
-                command = self.set_locale_pt_br
-            )
-        
-            return topbar_lang_menu
-        
-        def menu_sicknote(root:tk.Menu) -> tk.Menu:
-            # Creating a cascade menu with release of sick notes
-            topbar_sick_notes_menu = tk.Menu(root, tearoff=False)
-            # | New |
-            
-            topbar_sick_notes_menu.add_command(     #-# New sick note button
-                label   = translate['topbar_sicknote_new.button'],
-                command = self.frame_new_sick_note
-            )
-
-            return topbar_sick_notes_menu
-
-        # Creating the top menubar
-        top_bar = tk.Menu(self.window)
-
-        # Applying all cascade menus to the top menubar
-        top_bar.add_cascade(    #-# System cascade menu
-            label = translate['topbar_sys.topmenu'],
-            menu  = cascademenu_sys(top_bar)
-        )
-        top_bar.add_cascade(    #-# Sick notes cascade menu
-            label = translate['topbar_sicknote.topmenu'],
-            menu  = menu_sicknote(top_bar)
-        )
-
-        # Applying the menubar to the window
-        self.window.config(menu=top_bar)
-
-    # Gear Functions
-    def set_locale_en(self):
-        global translate_matrix, translate
-        translate = translate_matrix['en']
-        self.re_load()
-
-    def set_locale_pt_br(self):
-        global translate_matrix, translate
-        translate = translate_matrix['pt_br']
-        self.re_load()
-        
-    def re_load(self):
-        self.frame_welcome()
-        self.start_top_bar()
-        self.window.title(translate['window_title'])
-
-    def clear_frame(self) -> None:
-        if self.active_frame == None: return
-        else: self.active_frame.place_forget()
-
-    def new_frame(self) -> tk.Frame:
-        return tk.Frame(self.window,
-            width=self.win_width, height=self.win_height
-        )
-
-    
-
-    # Frames Functions
-    def frame_welcome(self) -> None:
-        self.clear_frame()
-
-        self.active_frame = self.new_frame()
-        label_center_x = (self.win_width/2 ) - 70
-        label_center_y = (self.win_height/2) - 25
-        
-        tk.Label(   #-# Central message of the frame
-            self.active_frame,
-            text = translate['frame_welcome.msg'],
-            font = ("Tahoma", 25)
-        ).place(x=label_center_x, y=label_center_y)
-        
-        self.active_frame.place(x=0, y=0)
-
-    def frame_new_sick_note(self) -> None:
-        self.clear_frame()
-        self.active_frame = self.new_frame()
+def update_translation():
+    global active_translation, translation_matrix, locale
+    global update_window, top_bar, clear_frame, frame_welcome
+    global save_config
 
 
-        pad_x = 5
-        top_frame = tk.Frame(self.active_frame,
-            width  = self.win_width-(pad_x*2),
-            height = 30,
-            relief = 'groove',
-            bg     = fnc.rgb_to_hex(112,128,144)
-        )
-        top_frame.place(x=pad_x, y=pad_x)
+    active_translation = translation_matrix[locale]
+    update_window()
+    top_bar()
+    clear_frame()
+    frame_welcome()
 
-        #-# Date label
-        tk.Label(top_frame,
-            text   = translate['frame_sicknote_new.date_label'],
-            font   = ("Tahoma", 10),
-            relief = 'flat',
-            bg=fnc.rgb_to_hex(112,128,144)
-        ).place(x=5, y=5)
+    save_config()
 
-        #-# Date entry
-        entry_date = tk.Entry(top_frame, width=11, justify='center')
-        entry_date.place(x=45, y=5)
-        entry_date.insert(0, fnc.get_today())
+
+#--# Frames
+active_frame:tk.Frame = None
+
+def clear_frame() -> None:
+    global active_frame
+
+    if active_frame == None: return
+    else: active_frame.place_forget()
+
+def new_frame() -> tk.Frame:
+    global window, win_width, win_height
+    return tk.Frame(window,
+        width  = win_width,
+        height = win_height
+    )
+
+def frame_welcome():
+    global active_frame, active_translation, window
+    global clear_frame, new_frame
+
+    clear_frame()
+    active_frame = new_frame()
+
+    text_label = active_translation['frame_welcome.msg']
+    label_center_x = (win_width/2 )-(len(text_label)*10)
+    label_center_y = (win_height/2) - 25
+
+    tk.Label(window,    #-# Central message of the frame.
+        text = text_label, font = ("Tahoma", 25)
+    ).place(x=label_center_x, y=label_center_y)
+
+    active_frame.place(x=0, y=0)
+
+def frame_new_sicknote():
+    global active_frame, active_translation, window, win_width
+    global clear_frame, new_frame
+
+    clear_frame()
+    active_frame = new_frame()
+
+    pad_x = 5
+    top_frame = tk.Frame(active_frame,
+        width   = win_width - (2 * pad_x),
+        height  = 30,
+        relief  = 'groove',
+        bg      = fnc.rgb_to_hex(112, 128, 144) 
+    )
+    top_frame.place(x=pad_x, y=pad_x)
+
+    #-# Date label
+    tk.Label(top_frame,
+        text    = active_translation['frame_sicknote_new.date_label'],
+        font    = ("Tahoma", 10),
+        relief  = 'flat',
+        bg      = fnc.rgb_to_hex(112, 128, 144)
+    ).place(x=5, y=5)
+
+    #-# Date entry
+    entry_date = tk.Entry(top_frame, width=11, justify='center')
+    entry_date.place(x=45, y=5)
+    entry_date.insert(0, fnc.get_today())
+
+
+
+    active_frame.place(x=0, y=0)
 
 
 
 
 
 
+load_config()
+update_translation()
 
-        self.active_frame.place(x=0, y=0)
-
-
-
-
+window.mainloop()
 
 
-
-
-
-Application()
