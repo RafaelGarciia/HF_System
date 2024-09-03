3cbe7ac54f4ae94fbc31216dd2561eceff048d1f
Rafael Garcia <TthanderBR@gmail.com>
2024-09-03 14:35:15 -0300

    Modularizando
    Modularizando a tradução do sistema para um arquivo separado.
    Com o objetivo de facilitar a manipulação das traduções.
    
    Movido os arquivos modulos, para uma pasta especifica.

---
+++ Source/modules/translation_module.py
@@ -0,0 +1,67 @@
+
+# Translation matrix
+
+ptbr_translation = {
+    "window.title"              : 'Sistema HF'   ,
+
+    "word.companie"             : 'Empresa'     ,
+    "word.employees"            : 'Funcionario' ,
+    "word.date"                 : 'Data'        ,
+    "word.sicknotes"            : 'Atestado'    ,
+    "word.new"                  : 'Novo'        ,
+    "word.list"                 : 'Lista'       ,
+    "word.save"                 : 'Salvar'      ,
+    "word.name"                 : 'Nome'        ,
+    "word.system"               : 'Sistema'     ,
+    "word.home"                 : 'Home'        ,
+    "word.language"             : 'Linguagem'   ,
+    "word.exit"                 : 'Sair'        ,
+
+    "error.name-empty"                  : 'O nome não pode ser vazio'   ,
+    "error.companie-empty"              : 'A empresa não pode ser vazia',
+    "error.companie-already-registered" : 'Empresa já registrada'       ,
+    "error.employee-already-registered" : 'Funcionario já registrado'   ,
+    
+    
+    "report.companie-success-registered"    : 'Empresa registrada com sucesso'    ,
+    "report.employee-success-registered"    : 'Funcionario registrado com sucesso',
+
+    "label.employee-name"       : 'Nome do funcionario',
+    
+    "msg.welcome"               : 'Bem vindo',
+}
+
+en_translation = {
+    "window.title"              : 'System HF'   ,
+
+    "word.companie"             : 'Companie'    ,
+    "word.employee"             : 'Employee'    ,
+    "word.date"                 : 'Date'        ,
+    "word.sicknote"             : 'Sick Note'   ,
+    "word.new"                  : 'New'         ,
+    "word.list"                 : 'List'        ,
+    "word.save"                 : 'Save'        ,
+    "word.name"                 : 'Name'        ,
+    "word.system"               : 'System'      ,
+    "word.home"                 : 'Home'        ,
+    "word.language"             : 'Language'    ,
+    "word.exit"                 : 'Exit'        ,
+
+    "error.name-empty"                  : 'The name cannot be empty'    ,
+    "error.companie-empty"              : 'The companie cannot be empty',
+    "error.companie-already-registered" : 'Companie already registered',
+    "error.employee-already-registered" : 'Employee already registered',
+    
+    
+    "report.companie-success-registered"    : 'Companie successfully registered',
+    "report.employee-success-registered"    : 'Employee successfully registered',
+
+    "label.employee-name"       : 'Employee Name',
+    
+    "msg.welcome"               : 'Welcome',
+}
+
+translation_matrix = {
+    "en"    : en_translation,
+    "pt_br" : ptbr_translation
+}
