a7dc8305866572f23b86bde78019071c1fe4fc90
Rafael Garcia <TthanderBR@gmail.com>
2025-06-03 14:24:52 -0300

    Create main.py
    Estruturado o esqueleto do sistema.
    Juntando todos os módulos.

---
+++ Source/main.py
@@ -0,0 +1,19 @@
+from window import Window
+from Frames import cadastro
+import sql
+
+sql.execute_script('sql_queries\\mk_database.sql')
+
+
+
+app = Window('Janela', 400, 400)
+app.config(menu=app.criar_menu({
+    "Sistema": {
+        "Sair": app.quit,
+    },
+    "Cadastro": {
+        "Material": lambda: app.show_frame(cadastro.Material),
+        "Empresa": lambda: app.show_frame(cadastro.Empresa),
+    }
+}))
+app.mainloop()
