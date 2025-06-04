2fbb42dbf9cf6dbe5cf43d17c128c203bfbc453a
Rafael Garcia <TthanderBR@gmail.com>
2025-06-04 13:38:00 -0300

    Menu cad fornecedor
    Adicionado no menu o cadastro de fornecedor

--- Source/main.py
+++ Source/main.py
@@ -5,15 +5,19 @@ import sql
 sql.execute_script('sql_queries\\mk_database.sql')
 
 
-
-app = Window('Janela', 400, 400)
-app.config(menu=app.criar_menu({
-    "Sistema": {
-        "Sair": app.quit,
-    },
-    "Cadastro": {
-        "Material": lambda: app.show_frame(cadastro.Material),
-        "Empresa": lambda: app.show_frame(cadastro.Empresa),
-    }
-}))
+app = Window('Janela', 500, 400)
+app.config(
+    menu=app.criar_menu(
+        {
+            'Sistema': {
+                'Sair': app.quit,
+            },
+            'Cadastro': {
+                'Material': lambda: app.show_frame(cadastro.Material),
+                'Empresa': lambda: app.show_frame(cadastro.Empresa),
+                'Fornecedor': lambda: app.show_frame(cadastro.Fornecedor),
+            },
+        }
+    )
+)
 app.mainloop()
